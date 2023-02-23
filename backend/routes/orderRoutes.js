6d6cb88eb2b145b0aad2ef2539a2eda16f6255c4
ahmed1995mansour <a.mansour@alxtel.com>
2023-02-23 09:41:54 +0200

    Added PayPal Integeraion

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,15 @@
 import express from 'express'
-import { addOrderItems, GetOrderById } from '../controllers/orderController.js'
+import {
+  addOrderItems,
+  GetOrderById,
+  UpdateOrderToPaid,
+} from '../controllers/orderController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.route('/').post(protect, addOrderItems)
 router.route('/:id').get(protect, GetOrderById)
+router.route('/:id/pay').put(protect, UpdateOrderToPaid)
 
 export default router
